7a103fd0e3fe886bdcd5cec05e03a8670765ba9c
Joe Averbukh <joeaverbukh@gmail.com>
2014-10-09 18:37:37 -0700

    Add first passing unit test and url mapping, dummy view

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -1,6 +1,9 @@
+from django.core.urlresolvers import resolve
 from django.test import TestCase
+from lists.views import home_page
 
-class SmokeTest(TestCase):
+class HomePageTest(TestCase):
 
-    def test_bad_math(self):
-        self.assertEqual(1,2)
+    def test_root_resolves_to_home_page(self):
+        found = resolve("/")
+        self.assertEqual(found.func, home_page)
