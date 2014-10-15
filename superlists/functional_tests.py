5184403a539c1f00b1dbea5d210b603cd14a7bec
Joe Averbukh <joeaverbukh@gmail.com>
2014-10-15 08:25:29 -0700

    Minor FT refactor

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -15,40 +15,42 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_row_in_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_start_and_retrieve_list(self):
         # Edith goes to home page
         self.browser.get('http://localhost:8000')
 
-        # Edith sees mention of to-do in page title and header
+        # She sees mention of to-do in page title and header
         self.assertIn('To-Do', self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do', header_text)
 
-        # There is an input area for Edith to type in a To-do
+        # There is an input area for her to type in a To-do
         inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertEqual(inputbox.get_attribute('placeholder'),
                          'Enter a to-do item')
 
-        # Edit types "Buy peacock feathers" into a text box
+        # She types "Buy peacock feathers" into a text box
         inputbox.send_keys('Buy peacock feathers')
-
-        # When Edith hits enter, the page updates, and now the page lists
-        # "1: Buy peacock feathers" as an item in a to-do list
         inputbox.send_keys(Keys.ENTER)
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feather', [row.text for row in rows])
 
-        # There is still an input area for Edith to type more To-dos
+        # the page updates, and shows the item in her to-do list
+        self.check_row_in_table('1: Buy peacock feathers')
+
+        # There is still an input area for her to type more To-dos
         inputbox = self.browser.find_element_by_id('id_new_item')
 
-        # Edith types "Buy more peacock feathers" and hits enter
+        # She types "Buy more peacock feathers" and hits enter
         inputbox.send_keys('Buy more peacock feathers')
         inputbox.send_keys(Keys.ENTER)
 
         # The page updates, and shows both items in her to-do list
-        self.assertIn('1: Buy peacock feather', [row.text for row in rows])
-        self.assertIn('2: Buy more peacock feathers', [row.text for row in rows])
+        self.check_row_in_table('1: Buy peacock feathers')
+        self.check_row_in_table('2: Buy more peacock feathers')
 
         # Edith wonders whether the site will remember her list. Then she sees
         # that the site has generated a unique URL for her -- there is some
