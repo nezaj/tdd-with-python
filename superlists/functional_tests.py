6743c208c58592aad8da60f5ab1b5d3512c41d10
Joe Averbukh <joeaverbukh@gmail.com>
2014-10-15 05:56:41 -0700

    Generate front page HTML from a template

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -24,9 +25,9 @@ class NewVisitorTest(unittest.TestCase):
         self.assertIn('To-Do', header_text)
 
         # There is an input area for Edith to type in a To-do
-        inputbox = self.browser.find_element_by_id('new-item')
+        inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertEqual(inputbox.get_attribute('placeholder'),
-                         'Enter a To-Do item')
+                         'Enter a to-do item')
 
         # Edit types "Buy peacock feathers" into a text box
         inputbox.send_keys('Buy peacock feathers')
@@ -37,7 +38,8 @@ class NewVisitorTest(unittest.TestCase):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows)
+            any(row.text == '1: Buy peacock feathers' for row in rows),
+            "New to-do item did not appear in the list"
         )
 
         # There is still an input area for Edith to type more To-dos
