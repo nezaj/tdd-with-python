4f74423a3f59d3232ed38bc8819fe2b0457411fa
Joe Averbukh <joeaverbukh@gmail.com>
2014-10-15 05:43:07 -0700

    Refactor home page view to use a template

--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -1,6 +1,5 @@
-# from django.shortcuts import render
+from django.shortcuts import render
 from django.http import HttpResponse
 
-# pylint: disable=unused-argument
 def home_page(request):
-    return HttpResponse('<html><title>To-Do lists</title></html>')
+    return render(request, 'home.html')
