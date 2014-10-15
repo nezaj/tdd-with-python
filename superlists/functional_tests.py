cc66eed11a20fa19f1a16da395510a72193a44a0
Joe Averbukh <joeaverbukh@gmail.com>
2014-10-15 05:11:17 -0700

    Add FT to check we can input a to-do item

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -14,22 +14,34 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def test_can_start_a_list_and_retrieve_it_later(self):
+    def test_start_and_retrieve_list(self):
         # Edith goes to home page
         self.browser.get('http://localhost:8000')
 
-        # Edith sees mention of to-do in page title
+        # Edith sees mention of to-do in page title and header
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish the test!')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
         # There is an input area for Edith to type in a To-do
+        inputbox = self.browser.find_element_by_id('new-item')
+        self.assertEqual(inputbox.get_attribute('placeholder'),
+                         'Enter a To-Do item')
 
         # Edit types "Buy peacock feathers" into a text box
+        inputbox.send_keys('Buy peacock feathers')
 
         # When Edith hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list
+        inputbox.send_keys(Keys.ENTER)
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy peacock feathers' for row in rows)
+        )
 
         # There is still an input area for Edith to type more To-dos
+        self.fail('Finish the test!')
 
         # Edith types "Buy more peacock feathers" and hits enter
 
