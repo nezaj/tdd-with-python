34ed2a57b5d7cc4d4d074da1986403584b29f823
Joe Averbukh <joeaverbukh@gmail.com>
2014-10-18 19:22:11 -0700

    Add new_list view

--- superlists/TODO.md
+++ superlists/TODO.md
@@ -1,5 +1,7 @@
+### In Progress
+* Add a url for creating lists via POST
+
 ### Backlog
 * Adjust model so that items are associated with different lists
 * Add unique URL for each list
-* Add a url for creating lists via POST
 * Add a url for creating new items for a list via a POST
--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -3,12 +3,12 @@ from django.shortcuts import redirect, render
 from lists.models import Item
 
 def home_page(request):
-    if request.method == 'POST':
-        Item.objects.create(text=request.POST['item_text'])
-        return redirect('/lists/one-list/')
-
     items = Item.objects.all()
     return render(request, 'home.html')
+
+def new_list(request):
+    Item.objects.create(text=request.POST['item_text'])
+    return redirect('/lists/one-list/')
 
 def view_list(request):
     items = Item.objects.all()
