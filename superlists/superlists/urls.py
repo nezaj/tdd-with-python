d9852c41a085679eb6c6349ecda01b9e25615aae
Joe Averbukh <joeaverbukh@gmail.com>
2014-10-18 18:51:34 -0700

    Add list view

--- superlists/superlists/urls.py
+++ superlists/superlists/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import patterns, url
 urlpatterns = patterns(
     '',
     url(r'^$', 'lists.views.home_page', name='home'),
+    url(r'^lists/one-list/$', 'lists.views.view_list', name='view_list'),
     # url(r'^blog/', include('blog.urls')),
 
     # url(r'^admin/', include(admin.site.urls)),
