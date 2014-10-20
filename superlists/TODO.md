c6c9a9ffa927fe47116585326f95bab5532e3862
Joe Averbukh <joeaverbukh@gmail.com>
2014-10-19 23:19:21 -0700

    Add List model and associate items with Lists

--- superlists/TODO.md
+++ superlists/TODO.md
@@ -1,7 +1,5 @@
 ### In Progress
-* Add a url for creating lists via POST
+* Add unique URL for each list
 
 ### Backlog
-* Adjust model so that items are associated with different lists
-* Add unique URL for each list
 * Add a url for creating new items for a list via a POST
--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import redirect, render
 
-from lists.models import Item
+from lists.models import Item, List
 
 def home_page(request):
     items = Item.objects.all()
     return render(request, 'home.html')
 
 def new_list(request):
-    Item.objects.create(text=request.POST['item_text'])
+    list_ = List.objects.create()
+    Item.objects.create(text=request.POST['item_text'], list=list_)
     return redirect('/lists/one-list/')
 
 def view_list(request):
